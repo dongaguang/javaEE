91644d12b5937825ca941b942d4ecf63412080e6
alex <18756003663@139.com>
2016-11-07 16:55:36 +0800

    111

---
+++ src/first/AsyncServlet.java
@@ -0,0 +1,14 @@
+package first;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * 异步的servlet
+ * @author Administrator
+ *
+ */
+//@WebServlet(urlPatterns:)
+public class AsyncServlet extends HttpServlet {
+
+}
