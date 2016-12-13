0a97f5b34de5f235d1dcc12327875bc7e69daa67
alex <18756003663@139.com>
2016-12-13 14:46:56 +0800

    第二个排序：选择排序

--- src/sort/SortAlgorithm.java
+++ src/sort/SortAlgorithm.java
@@ -11,6 +11,37 @@ import java.util.Random;
 public class SortAlgorithm {
 	
 	/**
+	 * 快速排序
+	 * 
+	 * @param array
+	 */
+	public void fastSort(int[] array){
+		
+	}
+	
+	/**
+	 * 选择排序
+	 * 它的工作原理是每一次从待排序的数据元素中选出最小（或最大）的一个元素，
+	 * 存放在序列的起始位置，直到全部待排序的数据元素排完
+	 * @param array
+	 */
+	public void selectedSort(int[] array){
+		for(int i = 0; i < array.length - 1; i++){//循环长度-1次
+			int min = array[i];//首次默认第一个数为最小
+			int index = i;
+			for(int j = i + 1; j < array.length; j++){//遍历接下来的所有数据，若比min小则记录这个数的下标
+				if(min > array[j]){
+					min = array[j];
+					index = j;
+				}
+			}
+			array[index] = array[i];
+			array[i] = min;
+			System.out.println("第" + i + "次比较后的数据：" + Arrays.toString(array));
+		}
+	}
+	
+	/**
 	 * 冒泡排序
 	 * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
 	 * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
@@ -19,10 +50,9 @@ public class SortAlgorithm {
 	 * @param array
 	 */
 	public void bubbleSort(int[] array){
-		for(int i = 1; i < array.length; i++){//外层循环控制循环次数
-//			System.out.println("第" + i + "次比较前的数据：" + Arrays.toString(array));
+		for(int i = 1; i < array.length; i++){//外层循环控制循环次数,共循环长度 - 1 次
 			for(int j = 0; j < array.length - i; j++){
-				if(array[j] > array[j + 1]){//前一个比后一大大，则交换位置
+				if(array[j] > array[j + 1]){//前一个比后一大大，则交换位置,即把大的数沉下去
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
@@ -40,6 +70,6 @@ public class SortAlgorithm {
 			array[i] = rm.nextInt(99);
 		}
 		System.out.println("           排序前的数组：" + Arrays.toString(array));
-		sa.bubbleSort(array);
+		sa.selectedSort(array);
 	}
 }
